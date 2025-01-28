c4005ff8abbcce31b3e21861aff03c7bb9a52ca4
5hes <553919500@qq.com>
2025-01-28 11:57:17 +0800

    Update setup.sh

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -8,3 +8,6 @@ curl -o zig.tar.xz $LINK
 tar -xf zig.tar.xz && rm zig.tar.xz
 mv * zig && cd zig && mkdir bin && mv zig bin && export PATH=$PATH:$PWD/bin
 cd ../..
+llvm-config --version
+export LD_LIBRARY_PATH=/usr/local/llvm/lib:$LD_LIBRARY_PATH
+export LLVM_CONFIG=/usr/local/llvm/bin/llvm-config

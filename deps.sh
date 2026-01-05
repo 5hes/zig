d0b85d7ad6864545cf5c1815d2d5420fe1086673
5hes <553919500@qq.com>
2026-01-05 23:50:40 +0800

    更新 deps.sh

--- deps.sh
+++ deps.sh
@@ -9,14 +9,14 @@ git clone --depth https://github.com/facebook/zstd.git
 
 cd $PREFIX/lib
 
-zig build-lib --name z \
-  -target $TARGET \
-  -fstrip -OReleaseFast \
-  -lc \
-  $(find $ROOT/zlib/ -name "*.c" | paste -sd " ")
-
 zig build-lib --name zstd \
   -target $TARGET \
   -fstrip -OReleaseFast \
   -lc \
   $(find $ROOT/zstd/ -name "*.c" | paste -sd " ")
+
+zig build-lib --name z \
+  -target $TARGET \
+  -fstrip -OReleaseFast \
+  -lc \
+  $(find $ROOT/zlib/ -name "*.c" | paste -sd " ")
